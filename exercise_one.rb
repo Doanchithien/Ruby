18f0cf41af493fc91b71765015e242d6dd314842
VuVT <truongvuuit@gmail.com>
2021-11-13 09:08:10 +0700

    Review exercise 1

--- exercise_one.rb
+++ exercise_one.rb
@@ -1,4 +1,4 @@
-arr = []
+arr = [] # mảng bao gồm nhiều phần tử, nên là số nhiều -> arrs
 puts "nhap -1 de dung nhap!"
 while arr.length <= 99 do
     begin
@@ -17,9 +17,11 @@ while arr.length <= 99 do
       end
 end
 puts "mang da nhap la : #{arr}"
-min=arr[0]
+min=arr[0] # Nên có dấu cách ra để dễ nhìn -> min = arr[0]
 arr.each do |i|
     min=i if i<min
 end
 
 puts "phan tu nho nhat trong mang la : #{min}"
+# Chương trình của e chạy tốt, nhưng để tốt hơn nữa thì con số 99 và -1 mình có thể thay đổi, lúc đó trong hàm của mình sẽ chỉnh sửa
+# nên e có thể đặt hằng số, vd: STOP_SIGNAL = -1 hay MAX_CAPACITY = 99
