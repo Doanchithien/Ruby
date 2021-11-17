4242220494b2acba025156d8fb14b8bfbae62973
thiendoan <doanchithien1118@gmail.com>
2021-11-17 16:02:27 +0700

    [EX 5-ThienDC] mang con tang dan

---
+++ exercise_five.rb
@@ -0,0 +1,35 @@
+def check_num(num)
+    begin
+        x = Integer(num)
+        true if x > 0 && x <=99
+    rescue
+        false
+    end
+end
+
+puts "Nhap so luong phan tu cua mang : "
+capacity_array = gets 
+until check_num(capacity_array)
+    puts "Nhap khong hop le,nhap lai : "
+    capacity_array = gets
+end
+
+arrays = Array.new(capacity_array.to_i) { |i| i = rand(1..9)}
+
+print "mang random la : #{arrays}\n"
+
+left = 0
+level = 1
+n = capacity_array.to_i - 1
+
+for i in 0..n
+    if i==n
+      print "mang con tang #{level} : #{arrays[left..i]}\n"
+    else    
+      if arrays[i] > arrays[i+1]
+         print "mang con tang #{level} : #{arrays[left..i]}\n"
+         left = i+1
+         level = level + 1
+      end
+    end
+end
