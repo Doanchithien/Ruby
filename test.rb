2e729a9a8730f21fb45c092401e626d88bab8626
VuVT <truongvuuit@gmail.com>
2021-11-19 09:41:20 +0700

    Review ex3

--- test.rb
+++ test.rb
@@ -10,3 +10,10 @@ CSV.foreach('test.csv', 'r') do |rows|
 end
 diff = Time.now - start
 db.exec("comment on table info is '#{diff}'")
+
+# Phần code e trình bày gọn, biết sử dụng foreach là một điểm cộng
+# db.exec, phần này a thấy e đang insert từng dòng vào trong db, tức là với 500k lines thì mình insert 500k lần về performance có thể bị ảnh hưởng 
+# do e hit vào db nhiều quá
+# A suggest e là 
+#   1. sử dụng transaction -> e có thể tìm hiểu nó là gì
+#   2. build ra 1 câu sql, nhưng khi nào đủ khoảng 5k lines (do e quyết định), rồi hẵn insert vào db. Như vậy sẽ giảm số lần e tương tác db ah
