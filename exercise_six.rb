7480448152d8c3b1e7b006bb7600c141c042006e
thiendoan <doanchithien1118@gmail.com>
2021-11-20 16:16:19 +0700

    [ThienDC-EX 6] complete

---
+++ exercise_six.rb
@@ -0,0 +1,16 @@
+target_sum = 10
+arrays =  [3, 5, -4, 8, 11, 1, -1, 6]
+n = arrays.length 
+hashes = {}
+two_nums = []
+for i in 0...n
+    num = arrays[i].to_i
+    check = target_sum - num
+    if hashes.has_key?(check)
+        two_nums.push(check,num)
+        break
+    else
+        hashes[num] = i
+    end
+end
+print "mang 2 so co tong bang #{target_sum} : #{two_nums}"
