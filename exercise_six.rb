9c378333fd3d8318d6aca01bdb257d6d1ae897f4
thiendoan <doanchithien1118@gmail.com>
2021-11-24 19:59:01 +0700

    [EX 6] Rename variable and divide code to each method

--- exercise_six.rb
+++ exercise_six.rb
@@ -1,16 +1,24 @@
-target_sum = 10
-arrays =  [3,5,-4,8,11,1,-1,6]
-n = arrays.length 
-hashes = {}
-two_nums = []
-for i in 0...n
-    num = arrays[i].to_i
-    check = target_sum - num
-    if hashes.has_key?(check)
-        two_nums.push(check,num)
+def two_num(arrays,target_sum)
+  number_hashes = {}
+
+  for i in 0...arrays.length 
+     number = arrays[i].to_i
+     number_check = target_sum - number
+     if number_hashes.has_key?(number_check)
+        return number_check,number
         break
-    else
-        hashes[num] = i
-    end
+     else
+        number_hashes[number] = i
+     end
+   end
+   
 end
-print "mang 2 so co tong bang #{target_sum} : #{two_nums}"
+
+
+
+if __FILE__ == $0
+  target_sum = 10
+  arrays =  [3,5,-4,8,11,1,-1,6]
+  print "Mang 2 so co tong bang #{target_sum} la : #{ two_num(arrays,target_sum)}"
+end
+  
