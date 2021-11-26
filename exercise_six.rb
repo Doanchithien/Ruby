15eefeb2ff5e0af23f11d8377eb7cd12f0f4a3ae
VuVT <truongvuuit@gmail.com>
2021-11-26 17:16:13 +0700

    Review ex6

--- exercise_six.rb
+++ exercise_six.rb
@@ -21,4 +21,8 @@ if __FILE__ == $0
   arrays =  [3,5,-4,8,11,1,-1,6]
   print "Mang 2 so co tong bang #{target_sum} la : #{ two_num(arrays,target_sum)}"
 end
-  
+# Code e viết gọn, đúng yêu cầu dùng 1 dòng for
+# A có vài góp ý: 
+# 1. Hàm khi e đã return rồi thì kg cần break nữa nhé
+# 2. Khi a nhập target_sum = 100 thì ra kq là 0...8 
+# Nếu không có giá trị nào trong mảng cộng lại mà = target_sum thì a thấy e chưa xử lý chổ này ah
