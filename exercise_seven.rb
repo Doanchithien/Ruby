c18f0530fe7ef4c9bdf80e5fdff6feffaeebb054
thiendoan <doanchithien1118@gmail.com>
2021-11-28 14:24:13 +0700

    [EX 7] customized

--- exercise_seven.rb
+++ exercise_seven.rb
@@ -5,7 +5,7 @@ def child_array?(arrays_one,arrays_two)
         hashes[arrays_two[i]] = i if i < arrays_two.length
         if hashes.has_key?(arrays_one[i])
             check_arrays.push(arrays_one[i])
-            check_arrays.pop() unless check_arrays == arrays_two[0...check_arrays.length]
+            check_arrays.pop() unless check_arrays[-1] == arrays_two[check_arrays.length - 1]
         end
     end
     check_arrays == arrays_two
@@ -15,9 +15,5 @@ end
 if __FILE__ == $0
     arrays_one = [1,5,22,-1,1,8,26,6,-1,1,8,1,10,9,10]
     arrays_two = [1,6,-1,8,1,9,10]
-    if child_array?(arrays_one,arrays_two)
-        print "Mang b la mang con cua mang a\n"
-    else
-        print "Mang b khong la mang con cua a\n"
-    end
+    print "Mang b la mang con cua a? : #{child_array?(arrays_one,arrays_two)}\n"
 end
