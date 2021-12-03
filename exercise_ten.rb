4277b45328e8f792c8430a5ce997f790da5e4298
thiendoan <doanchithien1118@gmail.com>
2021-12-03 08:13:42 +0700

    [EX 10] CUSTOMIZED

--- exercise_ten.rb
+++ exercise_ten.rb
@@ -1,11 +1,12 @@
-def check(i,numbers,target_sum,result)
+def check(length_config,numbers,target_sum,result)
   for j in numbers
-      $arr[i] = j 
-      if i == 2
+      $arr[length_config] = j 
+      if length_config == 2
           if $arr[0] == $arr[1] || $arr[0] == $arr[2] || $arr[1] == $arr[2]
               break
           else
               if $arr[0] + $arr[1] + $arr[2] == target_sum
+                  check_exist_result = true
                   if $arr[0] > $arr[1] && $arr[0] > $arr[2]
                       if $arr[1] > $arr[2]
                           result.push([$arr[2],$arr[1],$arr[0]])
@@ -28,7 +29,7 @@ def check(i,numbers,target_sum,result)
               end
           end
       else
-          check(i+1,numbers,target_sum,result)
+          check(length_config+1,numbers,target_sum,result)
       end
   end
 end
@@ -36,8 +37,13 @@ end
 if __FILE__==$0
   $arr = Array.new(3)
   numbers = [12, 3, 1, 2, -6, 5, -8, 6]
-  target_sum = 2
+  target_sum = 0
+  length_config = 0
   result = []
-  check(0,numbers,target_sum,result) 
-  result.uniq.each {|i| print "#{i}\n"}
+  check(length_config,numbers,target_sum,result) 
+  unless result.empty?
+    result.uniq.each {|i| print "#{i}\n"}
+  else
+    print []
+  end
 end
