4c18d156481186a06e3c6bb278c9de9068b8af02
Doanchithien <79781015+Doanchithien@users.noreply.github.com>
2021-12-03 08:46:01 +0700

    [EX 10] modified

--- exercise_ten.rb
+++ exercise_ten.rb
@@ -1,49 +1,48 @@
-def check(length_config,numbers,target_sum,result)
-  for j in numbers
-      $arr[length_config] = j 
-      if length_config == 2
-          if $arr[0] == $arr[1] || $arr[0] == $arr[2] || $arr[1] == $arr[2]
-              break
-          else
-              if $arr[0] + $arr[1] + $arr[2] == target_sum
-                  check_exist_result = true
-                  if $arr[0] > $arr[1] && $arr[0] > $arr[2]
-                      if $arr[1] > $arr[2]
-                          result.push([$arr[2],$arr[1],$arr[0]])
-                      else
-                          result.push([$arr[1],$arr[2],$arr[0]])
-                      end
-                  elsif $arr[1] > $arr[0] && $arr[1] > $arr[2]
-                      if $arr[0] > $arr[2]
-                          result.push([$arr[2],$arr[0],$arr[1]])
-                      else
-                          result.push([$arr[0],$arr[2],$arr[1]])
-                      end
-                  else
-                      if $arr[0] > $arr[1]
-                          result.push([$arr[1],$arr[0],$arr[2]])
-                      else
-                          result.push([$arr[0],$arr[1],$arr[2]])
-                      end
-                  end
-              end
-          end
-      else
-          check(length_config+1,numbers,target_sum,result)
-      end
+def check(length_config,numbers,target_sum,result,arrays)
+    for j in numbers
+        arrays[length_config] = j 
+        if length_config == 2
+            if arrays[0] == arrays[1] || arrays[0] == arrays[2] || arrays[1] == arrays[2]
+                break
+            else
+                if arrays[0] + arrays[1] + arrays[2] == target_sum
+                    if arrays[0] > arrays[1] && arrays[0] > arrays[2]
+                        if arrays[1] > arrays[2]
+                            result.push([arrays[2],arrays[1],arrays[0]])
+                        else
+                            result.push([arrays[1],arrays[2],arrays[0]])
+                        end
+                    elsif arrays[1] > arrays[0] && arrays[1] > arrays[2]
+                        if arrays[0] > arrays[2]
+                            result.push([arrays[2],arrays[0],arrays[1]])
+                        else
+                            result.push([arrays[0],arrays[2],arrays[1]])
+                        end
+                    else
+                        if arrays[0] > arrays[1]
+                            result.push([arrays[1],arrays[0],arrays[2]])
+                        else
+                            result.push([arrays[0],arrays[1],arrays[2]])
+                        end
+                    end
+                end
+            end
+        else
+            check(length_config+1,numbers,target_sum,result,arrays)
+        end
+    end
   end
-end
-
-if __FILE__==$0
-  $arr = Array.new(3)
-  numbers = [12, 3, 1, 2, -6, 5, -8, 6]
-  target_sum = 0
-  length_config = 0
-  result = []
-  check(length_config,numbers,target_sum,result) 
-  unless result.empty?
-    result.uniq.each {|i| print "#{i}\n"}
-  else
-    print []
+  
+  if __FILE__ == $0
+    arrays = Array.new(3)
+    numbers = [12, 3, 1, 2, -6, 5, -8, 6]
+    target_sum = 0
+    length_config = 0
+    result = []
+    check(length_config,numbers,target_sum,result,arrays) 
+    unless result.empty?
+      result.uniq.each {|i| print "#{i}\n"}
+    else
+      print []
+    end
   end
-end
