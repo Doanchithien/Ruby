403116c5a5d64bb59862f6bc9677c8d40c10861c
thiendoan <doanchithien1118@gmail.com>
2021-12-06 14:15:44 +0700

    [EX 10] divide function

--- exercise_ten.rb
+++ exercise_ten.rb
@@ -1,49 +1,57 @@
-def check(length_config,numbers,target_sum,result)
-  for j in numbers
-      $arr[length_config] = j 
-      if length_config == 2
-          if $arr[0] == $arr[1] || $arr[0] == $arr[2] || $arr[1] == $arr[2]
-              break
-          else
-              if $arr[0] + $arr[1] + $arr[2] == target_sum
-                  check_exist_result = true
-                  if $arr[0] > $arr[1] && $arr[0] > $arr[2]
-                      if $arr[1] > $arr[2]
-                          result.push([$arr[2],$arr[1],$arr[0]])
-                      else
-                          result.push([$arr[1],$arr[2],$arr[0]])
-                      end
-                  elsif $arr[1] > $arr[0] && $arr[1] > $arr[2]
-                      if $arr[0] > $arr[2]
-                          result.push([$arr[2],$arr[0],$arr[1]])
-                      else
-                          result.push([$arr[0],$arr[2],$arr[1]])
-                      end
-                  else
-                      if $arr[0] > $arr[1]
-                          result.push([$arr[1],$arr[0],$arr[2]])
-                      else
-                          result.push([$arr[0],$arr[1],$arr[2]])
-                      end
-                  end
-              end
-          end
-      else
-          check(length_config+1,numbers,target_sum,result)
-      end
-  end
+def sum(arrays)
+    arrays[0] + arrays[1] + arrays[2]
 end
 
-if __FILE__==$0
-  $arr = Array.new(3)
-  numbers = [12, 3, 1, 2, -6, 5, -8, 6]
-  target_sum = 0
-  length_config = 0
-  result = []
-  check(length_config,numbers,target_sum,result) 
-  unless result.empty?
-    result.uniq.each {|i| print "#{i}\n"}
-  else
-    print []
-  end
+def three_nums(arrays,target_sum)
+    result = []
+    #sort configs
+    arrays.each do |a|
+        unless a[0] == a[1] || a[0] == a[2] || a[1] == a[2]
+            if a[0] > a[1] && a[0] > a[2]
+                if a[1] > a[2]
+                    result.push([a[2],a[1],a[0]])
+                else
+                    result.push([a[1],a[2],a[0]])
+                end
+            elsif a[1] > a[0] && a[1] > a[2]
+                if a[0] > a[2]
+                    result.push([a[2],a[0],a[1]])
+                else
+                    result.push([a[0],a[2],a[1]])
+                end
+            else
+                if a[0] > a[1]
+                    result.push([a[1],a[0],a[2]])
+                else
+                    result.push([a[0],a[1],a[2]])
+                end
+            end
+        end
+    end
+    #remove duplicate configs and compare with target sum
+    result.uniq.each do |i|
+        print "#{i}\n" if sum(i) == target_sum
+    end
+end
+
+# function generate a config,three numbers per config
+def config_nums(length_config,numbers,arrays,configs)
+    for j in numbers
+        arrays[length_config] = j 
+        if length_config == 2
+            configs.push([arrays[0],arrays[1],arrays[2]])
+        else
+            config_nums(length_config+1,numbers,arrays,configs)
+        end
+    end
+end
+  
+if __FILE__ == $0
+    arrays = Array.new(3)
+    numbers = [12, 3, 1, 2, -6, 5, -8, 6]
+    target_sum = 0
+    length_config = 0
+    configs = []
+    config_nums(length_config,numbers,arrays,configs)
+    three_nums(configs,target_sum)
 end
